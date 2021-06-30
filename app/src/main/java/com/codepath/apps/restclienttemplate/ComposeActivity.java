71dda5e376c65b234909985ab8c404bce3d4548a
4tunekvng <56579818+4tunekvng@users.noreply.github.com>
2021-06-30 16:30:44 -0500

    some styling

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -2,6 +2,7 @@ package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +14,7 @@ import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONException;
+import org.parceler.Parcels;
 
 import okhttp3.Headers;
 
@@ -56,6 +58,13 @@ public class ComposeActivity extends AppCompatActivity {
                         try {
                             Tweet tweet = Tweet.fromJson(json.jsonObject);
                             Log.i(TAG, "Published Tweet says:"+ tweet.body);
+                            Intent intent = new Intent();
+                            intent.putExtra("tweet", Parcels.wrap(tweet));
+                            // set Result code and Bundle for response
+                            setResult(RESULT_OK, intent);
+                            // closes activity and passes the data to the parent
+                            finish();
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -5,6 +5,7 @@ import android.text.format.DateUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -12,15 +13,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+@Parcel
 public class Tweet {
     public String body;
     public String createdAt;
     public User user;
     public String tweetTime;
-    public JSONObject entities;
+    //public JSONObject entities;
     private boolean hasMedia;
-    public ArrayList<Object> embeddedImages;
-    public Object firstEmbeddedImage;
+    public ArrayList<String> embeddedImages;
+    public String firstEmbeddedImage;
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
@@ -28,9 +30,10 @@ public class Tweet {
         tweet.createdAt = jsonObject.getString("created_at");
         tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
         tweet.tweetTime= getRelativeTimeAgo(tweet.createdAt);
-        tweet.entities = jsonObject.getJSONObject("entities");
-        if (tweet.entities.has("media") ) {
-            JSONArray media = tweet.entities.getJSONArray("media");
+
+        JSONObject entities = jsonObject.getJSONObject("entities");
+        if (entities.has("media")) {
+            JSONArray media = entities.getJSONArray("media");
             tweet.hasMedia = true;
             tweet.embeddedImages = new ArrayList<>();
             for (int i = 0; i < media.length(); i++) {
